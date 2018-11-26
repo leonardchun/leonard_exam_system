7a861d2402ca1e381b2ea39bd1f832a57f95e644
wanghc <wanghc3@asiainfo.com>
2018-11-26 23:03:31 +0800

    邮件配置

--- leonard_exam_system_vue/src/views/modules/message/emailtemplate-config.vue
+++ leonard_exam_system_vue/src/views/modules/message/emailtemplate-config.vue
@@ -4,74 +4,18 @@
     :close-on-click-modal="false"
     :visible.sync="visible">
     <el-form :model="dataForm" :rules="dataRule" ref="dataForm" @keyup.enter.native="dataFormSubmit()" label-width="120px">
-      <el-form-item size="mini" label="存储类型">
-        <el-radio-group v-model="dataForm.type">
-          <el-radio :label="1">七牛</el-radio>
-          <el-radio :label="2">阿里云</el-radio>
-          <el-radio :label="3">腾讯云</el-radio>
-        </el-radio-group>
-      </el-form-item>
-      <template v-if="dataForm.type === 1">
-        <el-form-item size="mini">
-          <a href="http://www.renren.io/open/qiniu.html" target="_blank">免费申请(七牛)10GB储存空间</a>
+      <template >
+        <el-form-item label="发件服务器" prop="emailSendServer">
+          <el-input v-model="dataForm.emailSendServer" placeholder="smtp.163.com"></el-input>
         </el-form-item>
-        <el-form-item label="域名">
-          <el-input v-model="dataForm.qiniuDomain" placeholder="七牛绑定的域名"></el-input>
+        <el-form-item label="端口号" prop="emailPort">
+          <el-input v-model="dataForm.emailPort" placeholder="端口号"></el-input>
         </el-form-item>
-        <el-form-item label="路径前缀">
-          <el-input v-model="dataForm.qiniuPrefix" placeholder="不设置默认为空"></el-input>
+        <el-form-item label="邮箱账号" prop="emailNumber">
+          <el-input v-model="dataForm.emailNumber" placeholder="邮箱账号"></el-input>
         </el-form-item>
-        <el-form-item label="AccessKey">
-          <el-input v-model="dataForm.qiniuAccessKey" placeholder="七牛AccessKey"></el-input>
-        </el-form-item>
-        <el-form-item label="SecretKey">
-          <el-input v-model="dataForm.qiniuSecretKey" placeholder="七牛SecretKey"></el-input>
-        </el-form-item>
-        <el-form-item label="空间名">
-          <el-input v-model="dataForm.qiniuBucketName" placeholder="七牛存储空间名"></el-input>
-        </el-form-item>
-      </template>
-      <template v-else-if="dataForm.type === 2">
-        <el-form-item label="域名">
-          <el-input v-model="dataForm.aliyunDomain" placeholder="阿里云绑定的域名"></el-input>
-        </el-form-item>
-        <el-form-item label="路径前缀">
-          <el-input v-model="dataForm.aliyunPrefix" placeholder="不设置默认为空"></el-input>
-        </el-form-item>
-        <el-form-item label="EndPoint">
-          <el-input v-model="dataForm.aliyunEndPoint" placeholder="阿里云EndPoint"></el-input>
-        </el-form-item>
-        <el-form-item label="AccessKeyId">
-          <el-input v-model="dataForm.aliyunAccessKeyId" placeholder="阿里云AccessKeyId"></el-input>
-        </el-form-item>
-        <el-form-item label="AccessKeySecret">
-          <el-input v-model="dataForm.aliyunAccessKeySecret" placeholder="阿里云AccessKeySecret"></el-input>
-        </el-form-item>
-        <el-form-item label="BucketName">
-          <el-input v-model="dataForm.aliyunBucketName" placeholder="阿里云BucketName"></el-input>
-        </el-form-item>
-      </template>
-      <template v-else-if="dataForm.type === 3">
-        <el-form-item label="域名">
-          <el-input v-model="dataForm.qcloudDomain" placeholder="腾讯云绑定的域名"></el-input>
-        </el-form-item>
-        <el-form-item label="路径前缀">
-          <el-input v-model="dataForm.qcloudPrefix" placeholder="不设置默认为空"></el-input>
-        </el-form-item>
-        <el-form-item label="AppId">
-          <el-input v-model="dataForm.qcloudAppId" placeholder="腾讯云AppId"></el-input>
-        </el-form-item>
-        <el-form-item label="SecretId">
-          <el-input v-model="dataForm.qcloudSecretId" placeholder="腾讯云SecretId"></el-input>
-        </el-form-item>
-        <el-form-item label="SecretKey">
-          <el-input v-model="dataForm.qcloudSecretKey" placeholder="腾讯云SecretKey"></el-input>
-        </el-form-item>
-        <el-form-item label="BucketName">
-          <el-input v-model="dataForm.qcloudBucketName" placeholder="腾讯云BucketName"></el-input>
-        </el-form-item>
-        <el-form-item label="Bucket所属地区">
-          <el-input v-model="dataForm.qcloudRegion" placeholder="如：sh（可选值 ，华南：gz 华北：tj 华东：sh）"></el-input>
+        <el-form-item label="邮箱密码" prop="emailPassword">
+          <el-input v-model="dataForm.emailPassword" placeholder="邮箱密码"></el-input>
         </el-form-item>
       </template>
     </el-form>
@@ -87,8 +31,27 @@
     data () {
       return {
         visible: false,
-        dataForm: {},
-        dataRule: {}
+        dataForm: {
+          id: 0,
+          emailSendServer: '',
+          emailPort: '',
+          emailNumber: '',
+          emailPassword: ''
+        },
+        dataRule: {
+          emailSendServer: [
+            { required: true, message: '发件服务器不能为空', trigger: 'blur' }
+          ],
+          emailPort: [
+            { required: true, message: '端口号不能为空', trigger: 'blur' }
+          ],
+          emailNumber: [
+            { required: true, message: '邮箱账号不能为空', trigger: 'blur' }
+          ],
+          emailPassword: [
+            { required: true, message: '邮箱密码不能为空', trigger: 'blur' }
+          ]
+        }
       }
     },
     methods: {
