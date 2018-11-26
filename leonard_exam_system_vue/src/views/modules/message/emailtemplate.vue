10629671d8b7c4321db2ac03de37015e55ad53b3
wanghc <wanghc3@asiainfo.com>
2018-11-26 23:52:57 +0800

    邮件配置

--- leonard_exam_system_vue/src/views/modules/message/emailtemplate.vue
+++ leonard_exam_system_vue/src/views/modules/message/emailtemplate.vue
@@ -2,7 +2,7 @@
   <div class="mod-config">
     <el-form :inline="true" :model="dataForm" @keyup.enter.native="getDataList()">
       <el-form-item>
-        <el-input v-model="dataForm.key" placeholder="参数名" clearable></el-input>
+        <el-input v-model="dataForm.emailName" placeholder="邮件名称" clearable></el-input>
       </el-form-item>
       <el-form-item>
         <el-button @click="getDataList()">查询</el-button>
@@ -49,12 +49,6 @@
         align="center"
         width="300"
         label="邮件内容">
-      </el-table-column>
-      <el-table-column
-        prop="createUserId"
-        header-align="center"
-        align="center"
-        label="创建者">
       </el-table-column>
       <el-table-column
         prop="createTime"
@@ -135,7 +129,7 @@
           params: this.$http.adornParams({
             'page': this.pageIndex,
             'limit': this.pageSize,
-            'key': this.dataForm.key
+            'emailName': this.dataForm.emailName
           })
         }).then(({data}) => {
           if (data && data.code === 0) {
